7ca043fa2f8d26bca1f30f696ae9da45e9a70d8b
ehina24 <2240019@ecc.ac.jp>
2026-03-24 11:09:33 +0900

    add　ファイル追加

--- src/toppage.tsx
+++ src/toppage.tsx
@@ -1,7 +1,20 @@
+import { Link } from "react-router-dom";
+import '../src/css/toppage.css';
+import hydrangea from './assets/img/hydrangea.svg';
+
 function TOP(){
     return(
         <>
-        
+            <div className='contentWrap'>
+                <nav>
+                    <Link to="/works">Works</Link>
+                    <Link to="/about">About</Link>
+                    <Link to="/contact">Contact</Link>
+                </nav>
+
+                <h1>Hina's Portfolio</h1>
+                <img src={hydrangea} alt="紫陽花イラスト" />
+            </div>
         </>
     )
 }
