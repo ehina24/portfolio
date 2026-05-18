f9abfe078e54d2506d0a83974da240c506ef4ac1
ehina24 <2240019@ecc.ac.jp>
2026-05-18 14:10:20 +0900

    add　メインビジュアル追加

--- src/main.tsx
+++ src/main.tsx
@@ -1,8 +1,10 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+import './css/ress.css';
+import './css/index.css';
 import { BrowserRouter } from 'react-router-dom'
-import App from './App.tsx'
+import App from './App'
+
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
--- src/toppage.tsx
+++ src/toppage.tsx
@@ -2,33 +2,33 @@ import { Link } from "react-router-dom";
 import '../src/css/toppage.css';
 import hydrangea from './assets/img/hydrangea.svg';
 
+
 function TOP(){
     return(
         <>
             <div className='contentWrap'>
-                <nav>
+                <nav className="navWrap">
                     <Link to="/works">Works</Link>
                     <Link to="/about">About</Link>
                     <Link to="/contact">Contact</Link>
                 </nav>
 
-                <h1>Hina's Portfolio</h1>
-                <img src={hydrangea} alt="紫陽花イラスト" />
+            <div className="mainVisualWrap">
+                <h1>Hina's<br></br>Portfolio</h1>
+                <img src={hydrangea} alt="紫陽花イラスト" className="hydrangea"/>
+            </div>
 
                 <h2>Works</h2>
-                <h3>appeal works</h3>
-                
-                <div></div>
-                <p>Web</p>
-                <p>作品名</p>
-                <p>2000.9</p>
 
                 <div></div>
                 <p>Web</p>
                 <p>作品名</p>
                 <p>2000.9</p>
 
-                <h3>others</h3>
+                <div></div>
+                <p>Web</p>
+                <p>作品名</p>
+                <p>2000.9</p>
             </div>
         </>
     )
